7eee21c6cebe6f67647500dd30750c31a1168b28
Andrew Wittrock <andy@wittrock.us>
2017-04-06 22:55:52 -0500

    Create initial Redux rest module

---
+++ src/index.js
@@ -0,0 +1,91 @@
+/**
+ * Created by Andy on 3/24/2017.
+ */
+
+import request from "superagent";
+import Promise from "promise";
+
+import {addMessages} from "atp-flash";
+
+const BASE_URL = "http://api-atp.wittrock.us/rest/1.0/";
+
+const restCall = options =>
+    new Promise((resolve, reject) => {
+        return options.method.$switch({
+            get: () => request.get(BASE_URL + options.endPoint).query(options.data),
+            post: () => request.post(BASE_URL + options.endPoint).send(options.data),
+            put: () => request.put(BASE_URL + options.endPoint).send(options.data),
+            delete: () => request.delete(BASE_URL + options.endPoint),
+        })
+            .set('loginToken', options.loginToken)
+            .end((err, response) => {
+                if(options.dispatch && response.body.messages) {
+                    options.dispatch(addMessages(response.body.messages));
+                }
+                return err || !response.ok
+                    ? reject(err, response)
+                    : resolve(response.body)
+            })
+    });
+
+class Rest {
+    constructor() {
+        this.startHandler = () => {};
+        this.successHandler = null;
+        this.errorHandler = null;
+        this.method = null;
+        this.endPoint = null;
+        this.data = {};
+
+        this.then();
+    }
+
+    get(endPoint)   {this.method = 'get';    this.endPoint = endPoint; return this;}
+    post(endPoint)  {this.method = 'post';   this.endPoint = endPoint; return this;}
+    put(endPoint)   {this.method = 'put';    this.endPoint = endPoint; return this;}
+    delete(endPoint){this.method = 'delete'; this.endPoint = endPoint; return this;}
+
+    start(handler) {
+        this.startHandler = handler;
+
+        return this;
+    }
+
+    then(resolve, reject) {
+        this.successHandler = resolve || (() => {});
+        this.catch(reject);
+
+        return this;
+    }
+
+    catch(reject) {
+        this.errorHandler = reject || (() => {});
+
+        return this;
+    }
+
+    send(data) {
+        this.data = data;
+
+        return this;
+    }
+
+    thunk() {
+        return (dispatch, getState) => {
+            this.startHandler(this.data, dispatch, getState);
+            restCall({
+                endPoint: this.endPoint,
+                method: this.method,
+                data: this.data,
+                dispatch,
+                loginToken: getState().uac.loginToken
+            })
+                .then(data => this.successHandler(data, dispatch))
+                .catch(error => this.errorHandler(error, dispatch));
+        }
+    }
+
+}
+
+export default () => new Rest();
+export {restCall};
